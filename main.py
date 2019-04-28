42feeb8067e30934662e8cdbc7335bfe05b4ae7b
ianrandman <ixr5487@rit.edu>
2019-04-28 01:58:59 -0400

    Data from 8 subreddits gathered. Pipelining used for model fitting.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 import threading
-from time import sleep
 
 import praw
 from praw.models import MoreComments
@@ -10,10 +9,14 @@ from joblib import dump, load
 
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import fetch_20newsgroups
-from sklearn import svm
+
+from sklearn.pipeline import Pipeline
 
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
 TRAIN = False
 
@@ -29,9 +32,13 @@ reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,
 
 print(reddit.user.me())
 
-subreddit_names = ['nba', 'nhl', 'nfl']
-sub_to_num = {'r/nba': 0, 'r/nhl': 1, 'r/nfl': 2}
-num_to_sub = {0: 'r/nba', 1: 'r/nhl', 2: 'r/nfl'}
+subreddit_names = ['nba', 'nhl', 'nfl', 'mlb', 'soccer', 'formula1', 'CFB', 'sports']
+sub_to_num = {'r/nba': 0, 'r/nhl': 1, 'r/nfl': 2, 'r/mlb': 3, 'r/soccer': 4, 'r/formula1': 5, 'r/CFB': 6, 'r/sports': 7}
+num_to_sub = {0: 'r/nba', 1: 'r/nhl', 2: 'r/nfl', 3: 'r/mlb', 4: 'r/soccer', 5: 'r/formula1', 6: 'r/CFB', 7: 'r/sports'}
+
+# subreddit_names = ['nba', 'nhl', 'nfl']
+# sub_to_num = {'r/nba': 0, 'r/nhl': 1, 'r/nfl': 2}
+# num_to_sub = {0: 'r/nba', 1: 'r/nhl', 2: 'r/nfl'}
 
 
 def file_list(file_name):
@@ -52,74 +59,62 @@ def file_list(file_name):
     return f_list
 
 
-def parse_reddit_data():
+def parse_reddit_data(file_name):
     data = list()
     sub_classifications = list()
 
-    posts = file_list('train')
+    posts = file_list(file_name)
     for post in posts:
         post_split = post.split(',', 1)
 
-        data.append(post_split[1])
+        comments = post_split[1]
+        comments = comments.replace(' comment_separator ', ' ')
+        data.append(comments)
         sub_classifications.append(sub_to_num[post_split[0]])
 
     return data, sub_classifications
 
 
 def train():
-    # categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
-    # twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
+    data, sub_classifications = parse_reddit_data('training_data.txt')
+
+    text_clf = Pipeline([
+        ('vect', CountVectorizer()),
+        #('tfidf', TfidfTransformer(use_idf=False)),
+        ('clf', RandomForestClassifier()),
+        ])
+
+    # count_vect = CountVectorizer()
+    # counts = count_vect.fit_transform(data)
+    #
+    # tfidf_transformer = TfidfTransformer()
+    # tfidf = tfidf_transformer.fit_transform(counts)
     #
     #
-    # data = [' aa bb cc cc cc dd dd ee', 'ee ee ee dd cc bb bb aa']
-    #save_posts()
-    data, sub_classifications = parse_reddit_data()
+    # clf = MultinomialNB().fit(tfidf, sub_classifications)
 
-    count_vect = CountVectorizer()
-    counts = count_vect.fit_transform(data)
+    text_clf.fit(data, sub_classifications)
 
-    clf = MultinomialNB().fit(counts, sub_classifications)
-
-    dump(clf, 'classifier.joblib')
-    dump(count_vect, 'count_vectorizer.joblib')
+    dump(text_clf, 'classifier.joblib')
+    # dump(clf, 'classifier.joblib')
+    # dump(count_vect, 'count_vectorizer.joblib')
 
 
 def predict_sub():
-    clf = load('classifier.joblib')
-    count_vect = load('count_vectorizer.joblib')
+    text_clf = load('classifier.joblib')
+    #count_vect = load('count_vectorizer.joblib')
 
-    submissions = list()
-    test_data = list()
     test_subreddit_categories = list()
 
-    for subreddit_name in subreddit_names:
-        for submission in reddit.subreddit(subreddit_name).new(limit=20):
-            data = submission.selftext + ' '
-            for comment in submission.comments.list():
-                if isinstance(comment, MoreComments):
-                    continue
+    test_data, test_subreddit_categories = parse_reddit_data('development_data.txt')
 
-                data += comment.body + ' '
+    #new_counts = count_vect.transform(test_data)
 
-            data = data.replace('\n', ' ')
-            data = data.replace('\r', ' ')
-            
-            test_data.append(data)
-            test_subreddit_categories.append(sub_to_num[submission.subreddit_name_prefixed])
+    predicted = text_clf.predict(test_data)
 
-    random.shuffle(submissions)
-
-    for submission in submissions:
-        test_data.append(submission.title)
-        test_subreddit_categories.append(sub_to_num[submission.subreddit_name_prefixed])
-
-    new_counts = count_vect.transform(test_data)
-
-    predicted = clf.predict(new_counts)
-
-    for actual_sub, title, predicted_sub in zip(test_subreddit_categories, test_data, predicted):
+    for actual_sub, data, predicted_sub in zip(test_subreddit_categories, test_data, predicted):
         if actual_sub != predicted_sub:
-            print('%s: %r => %s' % (num_to_sub[actual_sub], title, num_to_sub[predicted_sub]))
+            print('%s: %s <= %r' % (num_to_sub[actual_sub], num_to_sub[predicted_sub], data))
 
     num_correct = 0
     num_total = 0
@@ -134,54 +129,8 @@ def predict_sub():
     print(np.mean(test_subreddit_categories == predicted))
 
 
-def save_posts(subreddit_name, thread_num, num_posts_to_get, num_total_posts):
-    # output = open('train', "w", encoding='utf-8')
-    # output.write("# this file contains all the data from the subreddits to be tested\n# subreddit_name, title\n\n")
-
-    output = ''
-
-    starting_post_num = thread_num * num_posts_to_get
-
-    post_number = 0
-    for submission in reddit.subreddit(subreddit_name).top(time_filter='all', limit=num_total_posts):
-        if starting_post_num <= post_number < starting_post_num + num_posts_to_get:
-            data = submission.selftext + ' comment_separator '
-            for comment in submission.comments.list():
-                if isinstance(comment, MoreComments):
-                    continue
-
-                data += comment.body + ' comment_separator '
-
-            data = data.replace('\n', ' ')
-            data = data.replace('\r', ' ')
-
-            output += submission.subreddit_name_prefixed + ',' + data + '\n'
-
-        post_number += 1
-
-    output_file = open(subreddit_name + '.txt', "a", encoding='utf-8')
-    output_file.write(output)
-
-        # output.write(submission.subreddit_name_prefixed + ',' + data + '\n')
-
-
 if __name__ == '__main__':
-    num_posts_to_get = 10
-    num_total_posts = 1000
-
-    threads = list()
-
-    for subreddit_name in subreddit_names:
-        for thread_num in range(int(num_total_posts / num_posts_to_get)):
-            print(thread_num)
-            thread = threading.Thread(target=save_posts, args=(subreddit_names[0], thread_num, num_posts_to_get, num_total_posts))
-            thread.start()
-            threads.append(thread)
-
-    for thread in threads:
-        thread.join()
-
-    # if TRAIN:
-    #     train()
-    # else:
-    #     predict_sub()
+    if TRAIN:
+        train()
+    else:
+        predict_sub()
